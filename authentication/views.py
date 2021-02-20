b0df992383a196322ec950aee00bc8278e19b4e7
Ayush Pratap Singh <apsingh1843@gmail.com>
2021-02-20 11:31:57 +0530

    second commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+from validate_email import validate_email
 
 # Create your views here.
+
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'invalid email'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'this email is already registered'}, status=419)
+
+        return JsonResponse({'email_valid': True})
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'username in use, choose another one'}, status=419)
+
+        return JsonResponse({'username_valid': True})
+
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'authentication/register.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
